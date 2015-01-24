b54d88693d5612f5935aa193f225c1d29c0a1014
tjlynch15 <terrencelynch@icloud.com>
2015-01-23 22:51:49 -0600

    First revision

--- divvy.py
+++ divvy.py
@@ -1,15 +1,54 @@
 # Divvy Bikes
 #
 # Here's an example of how to retrieve the list of Divvy bike stations:
-
+import math
 import json
 from urllib.request import urlopen
+
+def distance(lat, long):
+	dist = math.sqrt((41.793414 - lat)**2 + (-87.600915 - long)**2)
+	return dist
+
+
 
 webservice_url = "http://www.divvybikes.com/stations/json"
 data = urlopen(webservice_url).read().decode("utf8")
 result = json.loads(data)
 stations = result['stationBeanList']
-print(stations)
+#print(stations)
+
+
+distances = []
+for station in stations:
+	#print(station['stationName'])
+	#print(station['latitude'])
+	#print(station['longitude'])
+	#print(distance(station['latitude'], station['longitude']))
+	distances.append({'station': station['stationName'],
+	 'distance': distance(station['latitude'], station['longitude']),
+	  'available_docks': station['availableDocks']})
+
+
+min_dist = distances[0]['distance']
+min_station = distances[0]['station']
+min_bikes = distances[0]['available_docks']
+
+
+for entry in distances:
+	if entry['distance'] < min_dist:
+ 		min_dist = entry['distance']
+ 		min_station = entry['station']
+ 		min_bikes = entry['available_docks']
+
+#print(min_station, min_dist, min_bikes)
+print()
+print('The closest station to Young is: ', min_station)
+print('Available bikes are: ', min_bikes)
+print()
+
+
+
+
 
 # The Young building has the following latitude and longitude: 41.793414,-87.600915.
 # To measure surface distance, you can treat latitudes and longitudes like x and y coordinates, and calculate distance between locations with the usual Euclidean distance formula.
